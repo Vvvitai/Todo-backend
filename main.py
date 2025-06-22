b995a00e95e84700023f739612841b75a9c124a3
Vitosina <vitalialevchenko@mail.ru>
2025-06-22 21:22:47 +0300

    Дополнил описание приложения

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ class Settings(BaseSettings):
 
 
 settings = Settings()
-app = FastAPI(title='Todo backend', description='Приложение списка дел')
+app = FastAPI(title='Todo backend', description='Приложение списка дел, где есть рекурсия')
 
 origins = [
     "http://localhost:8000",
